3a1eb25f4514a33f0ee5b52b542f19d4d34efb3b
giorgi <giorgikutubidze@gmail.com>
2019-11-10 03:49:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(array){
   }
   let i = 0;
   while(i < array.length){
-    array[i] = 'The line is currently: ' + (i + 1)  + '. ' + array[i] + ',';
+    array[i] = 'The line is currently: ' + (i + 1)  + '. ' + array[i];
     i++;
   }
 
